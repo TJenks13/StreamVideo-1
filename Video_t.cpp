df01d3bc25b25fcc51ba93ad4b77d20677c3b5d2
TJenks13 <tjj12@zips.uakron.edu>
2017-12-12 15:03:56 -0500

    Add test case for movie

--- Video_t.cpp
+++ Video_t.cpp
@@ -10,5 +10,11 @@
 #include "Video.hpp"
 
 TEST_CASE( "NOP", "[Video]" ) {
+	Video video("The Lord of the Rings: The Fellowship of the Ring", 0, 3, 18, 0);
 
+	REQUIRE(video.getTitle() == "The Lord of the Rings: The Fellowship of the Ring");
+	REQUIRE(video.getType() == 0);
+	REQUIRE(video.getHours() == 3);
+	REQUIRE(video.getMinutes() == 18);
+	REQUIRE(video.getEpisodes() == 0);
 }
