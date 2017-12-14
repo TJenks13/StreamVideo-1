9e1d7d34f71128a7f2199f4256067a2f2b19c4fa
jat113 <jat113@zips.uakron.edu>
2017-12-14 06:00:15 -0500

    Performed Extract Subclass refactor on Stream
    Created unit test cases for new subclasses TVStream
    MovieStream and OriginalStream. These will be used
    in Account to kill switches. Also created
    virtual function getStreamCount for this purpose

--- Stream.hpp
+++ Stream.hpp
@@ -15,15 +15,52 @@ public:
     // constructor
     Stream(const Video& video, int occurrences);
 
+    // destructor
+    ~Stream() {}
     // number of times watched
     int getOccurrences() const;
 
     // video rented
     const Video& getVideo() const;
 
+    virtual int getStreamCount() const { return 0; };
+
 private:
     Video video;
     int occurrences;
+};
+
+class TvStream : public Stream
+{
+public:
+
+	// constructor
+	TvStream(const Video& video, int occurrences);
+
+	// for TV shows, the stream count is just the number of streams
+	int getStreamCount() const override;
+};
+
+class MovieStream : public Stream
+{
+public:
+
+	// constructor
+	MovieStream(const Video& video, int occurrences);
+
+	// For movies, stream count is the number of hours, with a minimum of 1
+	int getStreamCount() const override;
+};
+
+class OriginalStream : public Stream
+{
+public:
+
+	// constructor
+	OriginalStream(const Video& video, int occurrences);
+
+	// for TV shows, the stream count is just the number of streams
+	int getStreamCount() const override;
 };
 
 #endif
