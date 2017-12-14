3ac31e17799524876a6c2c47e789a2dd529bee52
ANBU BAPE OP <henry-n@users.noreply.github.com>
2017-12-14 00:50:52 -0500

    Added Test Cases For Stream_t.cpp

--- Stream_t.cpp
+++ Stream_t.cpp
@@ -9,6 +9,35 @@
 
 #include "Stream.hpp"
 
-TEST_CASE( "NOP", "[Stream]" ) {
+#include <iostream>
+
+TEST_CASE( "Test Occurrence Method", "[Stream]" ) {
+
+    //Create video object. Title is Star Wars
+    Video vid("Star Wars",1,1,1,1);
+
+    //Set occurrence of vid to 1
+    Stream vidInfo(vid,1);
+
+
+    //Test if function return proper value '1'
+    REQUIRE(vidInfo.getOccurrences() == 1);
 
 }
+
+TEST_CASE("Test getVideo() method")
+{
+    //Create video object. Title is Star Wars
+    Video vid("Star Wars",1,1,1,1);
+
+    //Create Stream Object
+    Stream vidInfo(vid,1);
+
+    //Create copy of vid object
+    Video vid2 = vidInfo.getVideo();
+
+    //Test if getVideo() returns "Star Wars" from vid2
+    REQUIRE(vid2.getTitle() == "Star Wars");
+
+}
+
