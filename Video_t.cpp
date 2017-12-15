79bafa9a9bc46c207504cafb973e7507d894f939
jat113 <jat113@zips.uakron.edu>
2017-12-14 22:27:02 -0500

    Added test case for video_t
    Test case added for when we construct a video using deserialized
    object

--- Video_t.cpp
+++ Video_t.cpp
@@ -8,7 +8,35 @@
 #include "catch.hpp"
 
 #include "Video.hpp"
+#include "DataRecord.hpp"
 
 TEST_CASE( "NOP", "[Video]" ) {
+	Video video("The Lord of the Rings: The Fellowship of the Ring", 0, 3, 18, 0);
+
+	REQUIRE(video.getTitle() == "The Lord of the Rings: The Fellowship of the Ring");
+	REQUIRE(video.getType() == 0);
+	REQUIRE(video.getHours() == 3);
+	REQUIRE(video.getMinutes() == 18);
+	REQUIRE(video.getEpisodes() == 0);
+}
+
+TEST_CASE( "Construct video with DataRecord", "[Video]")
+{
+	DataRecord dr;
+	dr.setTitle("Balto");
+	dr.setType("MOVIE");
+	dr.setHours(3);
+	dr.setMinutes(18);
+	dr.setEpisodes(0);
+
+	REQUIRE(dr.getType() == "MOVIE");
+
+	Video v(dr);
+
+	REQUIRE(v.getTitle() == "Balto");
+	REQUIRE(v.getType() == Video::MOVIE);
+	REQUIRE(v.getHours() == 3);
+	REQUIRE(v.getMinutes() == 18);
+	REQUIRE(v.getEpisodes() == 0);
 
 }
